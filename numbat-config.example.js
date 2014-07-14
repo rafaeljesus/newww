b935f3cf2204e76168856412e95b211df3425cd5
rockbot <raquel@rckbt.me>
2014-07-14 11:47:25 -0700

    adds numbat-config.example.js and keeps our seekrits secure :-P

---
+++ numbat-config.example.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  logging: {
+    name: 'numbat',
+    console: true
+  },
+
+  listen: { host: '0.0.0.0', port: 3333 },
+
+  outputs: [
+    {
+      type: 'dbtype',
+      hosts: [
+        { host: 'your-host-file',  port: 8086 },
+      ],
+      username: 'username',
+      password: 'password',
+      database: 'database'
+    },
+    { type: 'log', name: 'numbat-1' }
+  ]
+};

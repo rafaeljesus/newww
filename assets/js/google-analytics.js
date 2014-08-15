7ed75a5d2a8f38550e6114ee402042b68a98d472
rockbot <raquel@rckbt.me>
2014-08-15 11:10:39 -0700

    uses universal analytics

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -1,15 +1,19 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-47041310-1']);
-_gaq.push(['_trackPageview']);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-53856291-1', 'auto');
+ga('send', 'pageview');
 
 $(document).ready(function () {
   $('.hiring a').click(function (e) {
     var id = $(this).parent().data('id')
-    _gaq.push(['_trackEvent', 'Hiring Ads', 'click', id])
+    ga('send', 'event', 'Hiring Ads', 'click', id)
   })
 
   $('.npme-group a, .npme-details a').click(function (e) {
-    _gaq.push(['_trackEvent', 'npm Enterprise Beta', 'click'])
+    ga('send', 'event', 'npm Enterprise', 'click')
   })
 
 })

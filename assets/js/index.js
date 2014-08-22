905f6b8aca75da1cb2a7b8ba07c80d0878461237
Zeke Sikelianos <zeke@sikelianos.com>
2014-08-22 09:48:39 -0700

    register highlight.js languages in a DRY way

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,12 @@
 window.domready = require("domready");
 
-var Highlight = require('highlight.js/lib/highlight');
+var Highlight = require("highlight.js/lib/highlight");
 var hl = new Highlight();
-hl.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
-hl.registerLanguage('css', require('highlight.js/lib/languages/css'));
-hl.registerLanguage('coffeescript', require('highlight.js/lib/languages/coffeescript'));
-hl.registerLanguage('glsl', require('highlight.js/lib/languages/glsl'));
-hl.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
-hl.registerLanguage('http', require('highlight.js/lib/languages/http'));
-hl.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
-hl.registerLanguage('json', require('highlight.js/lib/languages/json'));
-hl.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
+"bash css coffeescript glsl http javascript json typescript xml"
+  .split(' ')
+  .forEach(function(lang){
+    hl.registerLanguage(lang, require('highlight.js/lib/languages/'+lang));
+  })
 
 domready(function() {
   console.log("DOM is ready");

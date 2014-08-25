dcd060ec72d19fb6baaf3eaa418d296e7f0f900f
Zeke Sikelianos <zeke@sikelianos.com>
2014-08-25 10:52:58 -0700

    use the schemeless npm package as a handlebars helper

--- hbs-helpers/schemeless.js
+++ hbs-helpers/schemeless.js
@@ -1,10 +1,1 @@
-module.exports = function schemeless (prop) {
-  var schemey = new RegExp("^https?:\/\/", "i")
-  if (typeof(prop) === "string" && prop.match(schemey)) {
-    return prop
-      .replace(schemey, '')
-      .replace(/\/$/, '')
-  } else {
-    return prop
-  }
-}
+module.exports = require("schemeless")

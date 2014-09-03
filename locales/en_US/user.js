a7a64e52663196b9b09f540c8e06e9c161efe3a5
rockbot <raquel@rckbt.me>
2014-09-03 16:46:50 -0700

    uses CSS to force lowercase for certain fields

--- locales/en_US/user.js
+++ locales/en_US/user.js
@@ -1,33 +1,33 @@
 module.exports = {
   signup: {
-    title: "create an account",
-    subtitle: "sign all the way up",
+    title: "Create an Account",
+    subtitle: "Sign all the way up",
     error: "There was a problem! The server said:",
     form: {
-      name: "username",
+      username: "Username",
       nameHelp: "Must be all lower-case, and not have any non-urlsafe chars",
-      password: "password",
-      verify: "verify password",
-      email: "email address",
+      password: "Password",
+      verify: "Verify Password",
+      email: "Email Address",
       emailHelp: "This <strong>will</strong> be public and shown on all the packages you publish. Accounts with invalid emails may be deleted without notice.",
-      submit: "make it so"
+      submit: "Make it so"
     }
   },
 
   login: {
-    title: "login",
-    subtitle: "you look lovely today, btw.",
+    title: "Login",
+    subtitle: "You look lovely today, btw.",
     error: {
       missing: "Missing username or password",
       invalid: "Invalid username or password",
       invalidContext: "If you feel that this is a mistake, please <a href='/contact'>contact us</a> with the following unique error ID:"
     },
     form: {
-      username: "username",
-      password: "password",
-      forgot: "forgot your password?",
-      signup: "create an account",
-      submit: "login"
+      username: "Username",
+      password: "Password",
+      forgot: "Forgot our password?",
+      signup: "Create an Account",
+      submit: "Login"
     }
   }
 };

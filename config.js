bf75189caea51e44f6f574ce8a9e99388f3cde7d
Zeke Sikelianos <zeke@sikelianos.com>
2014-09-17 10:40:54 -0700

    conditionally load the html5 “shiv” from cdnjs

--- config.js
+++ config.js
@@ -50,6 +50,7 @@ config.csp = {
     'self',
     'https://www.google-analytics.com',
     'https://fonts.googleapis.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js',
   ],
   styleSrc: [
     'self',

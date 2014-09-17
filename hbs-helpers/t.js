22b0019e1c9bb8df7f103947a57974cc207be2e8
Zeke Sikelianos <zeke@sikelianos.com>
2014-09-17 10:13:46 -0700

    Keep the translation helper simple by not passing the language around in all the templates.

--- hbs-helpers/t.js
+++ hbs-helpers/t.js
@@ -4,6 +4,10 @@ var locales = {
   en_US: require('../locales/en_US/index')
 };
 
-module.exports = function t (path, lang) {
+module.exports = function t (path) {
+  // US English is currently the only support langauge.
+  // Refactor this when we start supporting more languages.
+  var lang = "en_US"
+
   return Hoek.reach(locales[lang], path);
 };

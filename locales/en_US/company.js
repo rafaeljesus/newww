fab6ab7b5d1d75a586073881483a01b04b78e033
rockbot <raquel@rckbt.me>
2014-09-25 16:45:00 -0700

    adds whoshiring widget to package page

--- locales/en_US/company.js
+++ locales/en_US/company.js
@@ -2,6 +2,9 @@ module.exports = {
   whoshiring: {
     title: "Who's Hiring?",
     subtitle: "These companies are currently hiring for Node.js developers",
-    addCompany: "To add your company to this list, email <a href='mailto:whoshiring@npmjs.org'>whoshiring@npmjs.org</a> for pricing options."
+    addCompany: "To add your company to this list, email <a href='mailto:whoshiring@npmjs.org'>whoshiring@npmjs.org</a> for pricing options.",
+    more: "more",
+    companiesHiring: "companies are hiring",
+    viewAll: "View All"
   }
 }

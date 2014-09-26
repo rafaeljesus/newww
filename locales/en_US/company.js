7e0389ce2c50a695d392b6e42478f0bc0d283fce
Zeke Sikelianos <zeke@sikelianos.com>
2014-09-26 09:34:26 -0700

    tweak hiring copy

--- locales/en_US/company.js
+++ locales/en_US/company.js
@@ -1,7 +1,7 @@
 module.exports = {
   whoshiring: {
     title: "Who's Hiring?",
-    subtitle: "These companies are currently hiring for Node.js developers",
+    subtitle: "These companies are looking for JavaScript and Node.js developers",
     addCompany: "To add your company to this list, email <a href='mailto:whoshiring@npmjs.org'>whoshiring@npmjs.org</a> for pricing options.",
     more: "more",
     companiesHiring: "companies are hiring",

ef5b8ac84bc333e61b28cd60713bdc592ca371a2
Zeke Sikelianos <zeke@sikelianos.com>
2014-09-26 14:06:31 -0700

    only one HTTP verb per route, because the [‘GET’, ‘POST’] style breaks the test suite (but not individual tests). ¯\_(ツ)_/¯

--- routes.js
+++ routes.js
@@ -56,7 +56,7 @@ var routes = module.exports = [
 
   {
     path: "/joinwhoshiring",
-    method: ["GET", "POST"],
+    method: "GET",
     config: {
       handler: require('./facets/company/show-whoshiring-payments')(config.company.stripe),
       plugins: {
@@ -67,6 +67,21 @@ var routes = module.exports = [
       }
     }
   },
+
+  {
+    path: "/joinwhoshiring",
+    method: "POST",
+    config: {
+      handler: require('./facets/company/show-whoshiring-payments')(config.company.stripe),
+      plugins: {
+        blankie: {
+          scriptSrc: ['self', 'unsafe-eval', 'https://ssl.google-analytics.com', 'https://checkout.stripe.com'],
+          frameSrc: 'https://checkout.stripe.com'
+        }
+      }
+    }
+  },
+
 
   {
     path: "/npme-beta",
@@ -197,31 +212,92 @@ var routes = module.exports = [
 
   {
     path: "/signup",
-    method: ["GET", "HEAD", "POST"],
+    method: "GET",
+    handler: require('./facets/user/show-signup')
+  },
+
+  {
+    path: "/signup",
+    method: "HEAD",
+    handler: require('./facets/user/show-signup')
+  },
+
+  {
+    path: "/signup",
+    method: "POST",
     handler: require('./facets/user/show-signup')
   },
 
   {
     path: "/profile-edit",
-    method: ["GET", "HEAD", "PUT", "POST"],
+    method: "GET",
+    config: forceAuthConfig(require('./facets/user/show-profile-edit')(config.user.profileFields))
+  },
+
+  {
+    path: "/profile-edit",
+    method: "HEAD",
+    config: forceAuthConfig(require('./facets/user/show-profile-edit')(config.user.profileFields))
+  },
+
+  {
+    path: "/profile-edit",
+    method: "PUT",
+    config: forceAuthConfig(require('./facets/user/show-profile-edit')(config.user.profileFields))
+  },
+
+  {
+    path: "/profile-edit",
+    method: "POST",
     config: forceAuthConfig(require('./facets/user/show-profile-edit')(config.user.profileFields))
   },
 
   {
     path: "/email-edit",
-    method: ["GET", "HEAD", "PUT", "POST"],
+    method: "GET",
+    config: forceAuthConfig(require('./facets/user/show-email-edit')(config.user.mail))
+  },
+
+  {
+    path: "/email-edit",
+    method: "HEAD",
+    config: forceAuthConfig(require('./facets/user/show-email-edit')(config.user.mail))
+  },
+
+  {
+    path: "/email-edit",
+    method: "PUT",
+    config: forceAuthConfig(require('./facets/user/show-email-edit')(config.user.mail))
+  },
+
+  {
+    path: "/email-edit",
+    method: "POST",
     config: forceAuthConfig(require('./facets/user/show-email-edit')(config.user.mail))
   },
 
   {
     path: "/email-edit/{token*2}",
-    method: ["GET", "HEAD"],
+    method: "GET",
+    config: forceAuthConfig(require('./facets/user/show-email-edit')(config.user.mail))
+  },
+
+  {
+    path: "/email-edit/{token*2}",
+    method: "HEAD",
     config: forceAuthConfig(require('./facets/user/show-email-edit')(config.user.mail))
   },
 
   {
     path: "/login",
-    method: ["GET", "POST"],
+    method: "GET",
+    handler: require('./facets/user/show-login')
+  },
+
+
+  {
+    path: "/login",
+    method: "POST",
     handler: require('./facets/user/show-login')
   },
 
@@ -233,13 +309,37 @@ var routes = module.exports = [
 
   {
     path: "/password",
-    method: ["GET", "HEAD", "POST"],
+    method: "GET",
+    config: forceAuthConfig(require('./facets/user/show-password'))
+  },
+
+  {
+    path: "/password",
+    method: "HEAD",
+    config: forceAuthConfig(require('./facets/user/show-password'))
+  },
+
+  {
+    path: "/password",
+    method: "POST",
     config: forceAuthConfig(require('./facets/user/show-password'))
   },
 
   {
     path: "/forgot/{token?}",
-    method: ["GET", "HEAD", "POST"],
+    method: "GET",
+    handler: require('./facets/user/show-forgot')(config.user.mail)
+  },
+
+  {
+    path: "/forgot/{token?}",
+    method: "HEAD",
+    handler: require('./facets/user/show-forgot')(config.user.mail)
+  },
+
+  {
+    path: "/forgot/{token?}",
+    method: "POST",
     handler: require('./facets/user/show-forgot')(config.user.mail)
   },
 

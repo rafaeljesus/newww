be5be0d1d5d647e4085cda8cdc30e8291da1952f
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-15 15:48:01 -0700

    fix the blankie

--- adapters/plugins.js
+++ adapters/plugins.js
@@ -4,7 +4,7 @@ var config = require('../config');
 module.exports = [
   {
     plugin: require('crumb'),
-    options: { isSecure: true }
+    options: { cookieOptions: { isSecure: true } }
   },
   require('scooter'),
   {

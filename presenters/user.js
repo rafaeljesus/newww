35273b2c1af2a5e3346dcac1168968e6b091cd39
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-17 17:26:51 -0700

    add user presenter that will eventually supplant profile presenter entirely

--- presenters/user.js
+++ presenters/user.js
@@ -1,4 +1,61 @@
-module.exports = function(user) {
+var merge = require("lodash").merge
+var URL = require("url")
+var isURL = require("is-url")
+var atty = new RegExp("^@")
 
+module.exports = function(user) {
+  user = merge({}, user)
+  user.meta = deriveMetaObjectFromFieldsArray(user.fields)
   return user
+}
+
+var deriveMetaObjectFromFieldsArray = function(fields) {
+  var meta = {}
+
+  if (!Array.isArray(fields))
+    return meta
+
+  fields
+    .filter(function(field){
+      return field.name && field.value
+    })
+    .forEach(function(field){
+      if (field.name === "github") {
+        meta["github"] = sanitizeGitHubHandle(field.value)
+      }
+
+      if (field.name === "twitter") {
+        meta["twitter"] = sanitizeTwitterHandle(field.value)
+      }
+    })
+
+  return meta
+}
+
+var sanitizeTwitterHandle = function(input) {
+  // URL
+  if (isURL(input)) return URL.parse(input).path.replace("/", "")
+
+  // Not-fully-qualified URL
+  var twittery = new RegExp("^twitter.com/", "i")
+  if (input.match(twittery)) return input.replace(twittery, "")
+
+  // Starts with @
+  if (input.match(atty)) return input.replace(atty, "")
+
+  return input
+}
+
+var sanitizeGitHubHandle = function(input) {
+  // URL
+  if (isURL(input)) return URL.parse(input).path.replace("/", "")
+
+  // Not-fully-qualified URL
+  var githubby = new RegExp("^github.com/", "i")
+  if (input.match(githubby)) return input.replace(githubby, "")
+
+  // Starts with @
+  if (input.match(atty)) return input.replace(atty, "")
+
+  return input
 }
--- test/presenters/user.js
+++ test/presenters/user.js
@@ -8,57 +8,103 @@ var Lab = require('lab'),
     present = require(__dirname + "/../../presenters/user"),
     users = require(__dirname + "/../fixtures/users")
 
-describe("metadata", function () {
+
+describe("meta", function () {
 
   it("is an object with key-value pairs", function(done){
-    var user = present(users.fakeuser)
+    var user = present(users.full_meta)
     expect(user.meta).to.exist
     expect(user.meta).to.be.an("Object")
     done()
   })
 
-  // it("removes meta pairs with empty values", function(done){
-  //
-  // })
-  //
-  // describe("github", function () {
-  //
-  //   it("removes leading @ from username if present", function(done){
-  //
-  //   })
-  //
-  //   it("extracts username if value is a URL", function(done){
-  //
-  //   })
-  //
-  //   it("generates a URL to the user's profile page", function(done){
-  //
-  //   })
-  //
-  // })
-  //
-  // describe("twitter", function () {
-  //
-  //   it("removes leading @ from username if present", function(done){
-  //
-  //   })
-  //
-  //   it("extracts username if value is a URL", function(done){
-  //
-  //   })
-  //
-  //   it("generates a URL to the user's profile page", function(done){
-  //
-  //   })
-  //
-  // })
-  //
-  // describe("email", function () {
-  //
-  //   it("generates a mailto URL", function(done){
-  //
-  //   })
-  //
-  // })
+  it("removes meta pairs with empty values", function(done){
+    var user = present({
+      name: "mona",
+      fields: [
+        {name: "github", value: "mona"},
+        {name: "twitter", value: "mona"},
+        {name: "", value: "mona"},
+        {name: "ICQ", value: ""}
+      ]
+    })
+    expect(Object.keys(user.meta)).to.have.length(2)
+    expect(user.meta.github).to.exist
+    expect(user.meta.twitter).to.exist
+    done()
+  })
 
+  describe("github", function () {
+
+    it("removes leading @ from username if present", function(done){
+      var user = present({
+        name: "eleanor",
+        fields: [
+          {name: "github", value: "@eleanor"},
+        ]
+      })
+      expect(user.meta.github).to.equal("eleanor")
+      done()
+    })
+
+    it("extracts username if value is a URL", function(done){
+      var user = present({
+        name: "suzan",
+        fields: [
+          {name: "github", value: "https://github.com/suzan"},
+        ]
+      })
+      expect(user.meta.github).to.equal("suzan")
+      done()
+    })
+
+    it("extracts username if value is a schemeless URL", function(done){
+      var user = present({
+        name: "jimbo",
+        fields: [
+          {name: "github", value: "github.com/jimbo"},
+        ]
+      })
+      expect(user.meta.github).to.equal("jimbo")
+      done()
+    })
+
+  })
+
+  describe("twitter", function () {
+
+    it("removes leading @ from username if present", function(done){
+      var user = present({
+        name: "eleanor",
+        fields: [
+          {name: "twitter", value: "@eleanor"},
+        ]
+      })
+      expect(user.meta.twitter).to.equal("eleanor")
+      done()
+    })
+
+    it("extracts username if value is a URL", function(done){
+      var user = present({
+        name: "suzan",
+        fields: [
+          {name: "twitter", value: "https://twitter.com/suzan"},
+        ]
+      })
+      expect(user.meta.twitter).to.equal("suzan")
+      done()
+    })
+
+    it("extracts username if value is a schemeless URL", function(done){
+      var user = present({
+        name: "suzan",
+        fields: [
+          {name: "twitter", value: "twitter.com/suzan"},
+        ]
+      })
+      expect(user.meta.twitter).to.equal("suzan")
+      done()
+    })
+
+  })
 })

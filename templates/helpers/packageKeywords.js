e7496440968f47853d79d9487b0d69342cc05e09
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-27 14:50:47 -0700

    modify handlebars keywords helper to allow a `count` argument

--- templates/helpers/packageKeywords.js
+++ templates/helpers/packageKeywords.js
@@ -1,13 +1,17 @@
-module.exports = function packageKeywords (keywords) {
+module.exports = function packageKeywords(keywords, count) {
   if (typeof keywords === 'string') {
     keywords = keywords.split(/\s*,?\s*/)
   }
 
+  if (!count) {
+    count = 100
+  }
+
   if (Array.isArray(keywords)) {
-    keywords = keywords.map(function (kw) {
+    keywords = keywords.slice(0,count).map(function(kw) {
       kw = kw.replace(/</g, '&lt;').replace(/"/g, '&quot;')
-            return '<a href="/browse/keyword/' + kw + '">' + kw + '</a>'
-      }).join(', ')
+      return '<a href="/browse/keyword/' + kw + '">' + kw + '</a>'
+    }).join(', ')
   }
 
   return keywords;

6bfe4ad4325fbd2889baf611b0f0c25234d8cfa6
Zeke Sikelianos <zeke@sikelianos.com>
2014-11-05 17:02:03 -0800

    use a magic trick to get github issue count with a single API call

--- assets/scripts/update-package-issue-count.js
+++ assets/scripts/update-package-issue-count.js
@@ -1,48 +1,47 @@
+var parseLinkHeader = require('parse-link-header')
+
 module.exports = function(){
 
   window.issuesEl = $("#issues")
 
-  if (issuesEl.length && issuesEl.data().ghapi) {
-    var issuesURL = issuesEl.data().ghapi+"/issues?per_page=100"
-    $.getJSON(issuesURL, function() {
-    })
-    .done(function(issues) {
-      if (!issues.length) return
-      window.issues = issues
-      window.pulls = issues.filter(function(i) { return i.pull_request })
-      var count = issues.length - pulls.length
-      var label
-
-      switch (count) {
-        case 0:
-          label = "No open issues"
-          break;
-        case 1:
-          label = "One open issue"
-          break;
-        default:
-          label = count + " open issues"
-          break;
-      }
-
-      if (count>49) {
-        label = "50+ open issues"
-      }
-
-      // switch (pulls.length) {
-      //   case 0:
-      //     break;
-      //   case 1:
-      //     label += " and one pull request"
-      //     break;
-      //   default:
-      //     label += " and " + pulls.length + " pull requests"
-      //     break;
-      // }
-
-      issuesEl.find(".original").hide()
-      issuesEl.find(".open_issues_count").text(label)
-      issuesEl.find(".enhanced").show()
-    })
+  if (!issuesEl.length || !issuesEl.data() || !issuesEl.data().ghapi) {
+    return
   }
+
+  var issuesURL = issuesEl.data().ghapi+"/issues?per_page=1"
+  console.log(issuesURL)
+
+  $.getJSON(issuesURL)
+  .done(function(issues, textStatus, xhr) {
+    window.issues_xhr = xhr
+    var label
+    var count
+
+    try {
+      count = Number(parseLinkHeader(xhr.getResponseHeader("Link")).last.page)
+    } catch (er) {
+      count = issues.length
+    }
+
+    if (!count) {
+      return
+    }
+
+    switch (count) {
+      case 0:
+        label = "No open issues"
+        break;
+      case 1:
+        label = "One open issue"
+        break;
+      default:
+        label = count + " open issues and PRs"
+        break;
+    }
+
+    issuesEl.find(".original").hide()
+    issuesEl.find(".open_issues_count").text(label)
+    issuesEl.find(".enhanced").show()
+  })
+
 }

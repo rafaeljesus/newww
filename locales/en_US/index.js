e24ad50043fc1ecb65dfd309bcf404143c35aca4
Zeke Sikelianos <zeke@sikelianos.com>
2014-11-10 20:42:52 -0800

    a new take on the package 404 page

--- locales/en_US/index.js
+++ locales/en_US/index.js
@@ -2,7 +2,6 @@ exports.user = require('./user.js');
 exports.company = require('./company.js')
 exports.package = {
   not_found: {
-    title: "Package not found",
-    subtitle: "Sometimes a 404 is a good thing"
+    title: "not found"
   }
 }

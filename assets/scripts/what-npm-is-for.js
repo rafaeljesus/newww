e00b5eb52ca8e4358504dedc9889507ced1c7edb
Zeke Sikelianos <zeke@sikelianos.com>
2014-11-13 22:42:54 -0800

    make the malarkey more future-friendly

--- assets/scripts/what-npm-is-for.js
+++ assets/scripts/what-npm-is-for.js
@@ -2,32 +2,31 @@ var $ = require("jquery");
 window.malarkey = require("malarkey");
 
 module.exports = function(){
-  $(function() {
+  $(what_npm_is_for)
+}
 
-    // Start by retyping the text content present in
-    // the element at load time
-    var elem = document.querySelector('#what-npm-is-for');
-    var initialText = elem.textContent;
+var what_npm_is_for = function() {
+  var el = document.querySelector('#what-npm-is-for');
+  if (!el) return;
+  var initialText = el.textContent;
+  var pause = 1200
+  var opts = {
+    speed: 50,
+    loop: false,
+    postfix: ''
+  };
 
-    var pause = 1200
-    var opts = {
-      speed: 50,
-      loop: false,
-      postfix: ''
-    };
-
-    malarkey(elem, opts)
-      .clear()
-      .type(initialText).pause(pause).delete(10)
-      .type('node').pause(pause).delete(4)
-      .type('browsers').pause(pause).delete(8)
-      .type('angular').pause(pause).delete(7)
-      .type('bower').pause(pause).delete(5)
-      .type('gulp').pause(pause).delete(4)
-      .type('browserify').pause(pause).delete(10)
-      .type('grunt').pause(pause).delete(5)
-      .type('tessel').pause(pause).delete(6)
-      .type('you.').pause(600).delete(1)
-      .type(', Buddy.');
-  })
+  malarkey(el, opts)
+    .clear()
+    .type(initialText).pause(pause).delete(10)
+    .type('node').pause(pause).delete(4)
+    .type('browsers').pause(pause).delete(8)
+    .type('angular').pause(pause).delete(7)
+    .type('bower').pause(pause).delete(5)
+    .type('gulp').pause(pause).delete(4)
+    .type('browserify').pause(pause).delete(10)
+    .type('grunt').pause(pause).delete(5)
+    .type('tessel').pause(pause).delete(6)
+    .type('you.').pause(600).delete(1)
+    .type(', Buddy.');
 }

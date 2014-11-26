1138093d634b98af2d0dc9c0fbb3303613bed0f4
Zeke Sikelianos <zeke@sikelianos.com>
2014-11-25 17:04:34 -0800

    npm is for many things, but node shows up first and javascript shows up last and stays on the screen

--- assets/scripts/what-npm-is-for.js
+++ assets/scripts/what-npm-is-for.js
@@ -9,6 +9,7 @@ var what_npm_is_for = function() {
   var el = document.querySelector('#what-npm-is-for');
   if (!el) return;
   var initialText = el.textContent;
+  var initialWord = initialText.match(/ (\w+)$/)[1];
   var pause = 800
   var opts = {
     speed: 40,
@@ -18,15 +19,15 @@ var what_npm_is_for = function() {
 
   malarkey(el, opts)
     .clear()
-    .type(initialText).pause(pause).delete(10)
-    .type('node').pause(pause).delete(4)
+    .type(initialText).pause(pause).delete(initialWord.length)
     .type('browsers').pause(pause).delete(8)
+    .type('mobile').pause(pause).delete(6)
     .type('angular').pause(pause).delete(7)
     .type('bower').pause(pause).delete(5)
     .type('gulp').pause(pause).delete(4)
     .type('browserify').pause(pause).delete(10)
     .type('grunt').pause(pause).delete(5)
     .type('tessel').pause(pause).delete(6)
-    .type('you.').pause(600).delete(1)
-    .type(', friend.');
+    .type('JavaScript.');
+
 }

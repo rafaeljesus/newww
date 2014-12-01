bf4b38ef6f03d3b0e100463dbb1f243af83b60ba
Laurie Voss <git@seldo.com>
2014-12-01 14:44:21 -0800

    Maybe not so much with the console.logs everywhere

--- assets/scripts/private-npm-beta.js
+++ assets/scripts/private-npm-beta.js
@@ -1,7 +1,5 @@
 module.exports = function() {
-  console.log("I am happening")
   $(function() {
-    console.log("I, too, am happening")
     hbspt.forms.create({
       portalId: '419727',
       formId: 'd9ba17d5-606e-456d-a703-733c67f5e708',

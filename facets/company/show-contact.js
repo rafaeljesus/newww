9178661a38a1d4bb9bfde0ac068576f90701c342
rockbot <raquel@rckbt.me>
2014-12-02 13:28:45 -0800

    updates humans on contact page to use npm-humans

--- facets/company/show-contact.js
+++ facets/company/show-contact.js
@@ -1,53 +1,16 @@
-var gravatar = require('gravatar').url;
+var gravatar = require('gravatar').url,
+    humans = require('npm-humans'),
+    _ = require('lodash');
 
 module.exports = function (request, reply) {
 
-  var maintainers = [
-    {
-      username: 'isaacs',
-      email: 'i@izs.me'
-    },
-    {
-      username: 'seldo',
-      email: 'laurie@npmjs.com'
-    },
-    {
-      username: 'rod',
-      email: 'rod.boothby@gmail.com'
-    },
-    {
-      username: 'rockbot',
-      email: 'raquel@rckbt.me'
-    },
-    {
-      username: 'ceejbot',
-      email: 'ceejceej@gmail.com'
-    },
-    {
-      username: 'bcoe',
-      email: 'bencoe@gmail.com'
-    },
-    {
-      username: 'othiym23',
-      email: 'ogd@aoaioxxysz.net'
-    },
-    {
-      username: 'zeke',
-      email: 'zeke@sikelianos.com'
-    },
-    {
-      username: 'iarna',
-      email: 'me@re-becca.org'
-    }
-  ];
-
-  maintainers.forEach(function (m) {
-    m.avatar = gravatar(m.email, {s:100, d:'retro'}, true);
+  humans = _.each(humans, function (m) {
+    return m.avatar = gravatar(m.email, {s:100, d:'retro'}, true);
   });
 
   var opts = {
     user: request.auth.credentials,
-    maintainers: maintainers
+    humans: humans
   };
 
   return reply.view('company/contact', opts);

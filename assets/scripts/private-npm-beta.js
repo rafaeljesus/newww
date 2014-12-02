24cbb70ba7356fcb3a195eb718d6ca604d621a15
rockbot <raquel@rckbt.me>
2014-12-01 17:09:38 -0800

    fixes little hbspt bug

--- assets/scripts/private-npm-beta.js
+++ assets/scripts/private-npm-beta.js
@@ -1,9 +1,11 @@
 module.exports = function() {
   $(function() {
-    hbspt.forms.create({
-      portalId: '419727',
-      formId: 'd9ba17d5-606e-456d-a703-733c67f5e708',
-      target: '#private-module-signup-form'
-    })
+    if (typeof hbspt !== 'undefined') {
+      hbspt.forms.create({
+        portalId: '419727',
+        formId: 'd9ba17d5-606e-456d-a703-733c67f5e708',
+        target: '#private-module-signup-form'
+      })
+    }
   })
 }

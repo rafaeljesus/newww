8b57b03ab40fbfc89abac365343fd7a8cbba84f8
Zeke Sikelianos <zeke@sikelianos.com>
2014-12-10 16:44:57 -0800

    inject stripe public key in a data attribute instead of unsafe-inline

--- assets/scripts/stripe.js
+++ assets/scripts/stripe.js
@@ -1,4 +1,6 @@
-var $ = require("jquery")
+var $ = require("jquery");
+var dataEl;
+var stripePublicKey;
 
 module.exports = function() {
   $(handleBillingForm)
@@ -9,7 +11,14 @@ var handleBillingForm = function() {
   // External stripe scripts are not loaded on all pages
   if (typeof Stripe === "undefined") return;
 
-  console.log("Stripe", Stripe)
+  try {
+    stripePublicKey = $("#billing-form").data('stripePublicKey');
+  } catch(e) {
+    console.error("No element with found with data-stripe-public-keys attribute");
+    return;
+  }
+
+  Stripe.setPublishableKey(stripePublicKey);
 
   var stripeResponseHandler = function() {
     console.log(arguments)
@@ -18,10 +27,14 @@ var handleBillingForm = function() {
   $('#billing-form').submit(function(event) {
     var $form = $(this);
 
-    // Disable the submit button to prevent repeated clicks
-    // $form.find('input[type=submit]').prop('disabled', true);
+    console.log("submitting")
 
-    // Stripe.card.createToken($form, stripeResponseHandler);
+    // Disable the submit button to prevent repeated clicks
+    $form.find('input[type=submit]').prop('disabled', true);
+
+    Stripe.card.createToken($form, stripeResponseHandler);
+
+    console.log("submitted")
 
     // Prevent the form from submitting with the default action
     return false;

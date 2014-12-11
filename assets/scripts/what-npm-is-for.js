75edf19bc426e32e4bc62310ed162571b0283d53
Benjamin Lupton <b@lupton.cc>
2014-12-11 17:39:07 +1100

    Added docpad to what npm is for
    DocPad's plugin system is powered by npm: https://www.npmjs.com/search?q=docpad-plugin and was one of the first (if not the first) plugin system for node

--- assets/scripts/what-npm-is-for.js
+++ assets/scripts/what-npm-is-for.js
@@ -33,6 +33,7 @@ var what_npm_is_for = function() {
     .type('browserify').pause(pause).delete(10)
     .type('grunt').pause(pause).delete(5)
     .type('tessel').pause(pause).delete(6)
+    .type('docpad').pause(pause).delete(6)
     .type('JavaScript.').pause(1200)
     .call(function() {
       $(el).addClass("disabled")

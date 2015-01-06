ca080976782fe47cc9da3b39c4311f1f6ce5d9de
Zeke Sikelianos <zeke@sikelianos.com>
2015-01-06 12:28:02 -0800

    hide billing form by default for existing customers

--- assets/scripts/billing-cancel.js
+++ assets/scripts/billing-cancel.js
@@ -3,8 +3,16 @@ var $ = require("jquery");
 module.exports = function() {
   $(function(){
     $(document).on("click", '#cancel-subscription-toggler', function(){
-      $("#cancel-subscription").slideToggle()
+      $("#cancel-subscription").toggle()
+      $("#payment-form").hide()
       return false;
     })
+
+    $(document).on("click", '#update-subscription-toggler', function(){
+      $("#cancel-subscription").hide()
+      $("#payment-form").toggle()
+      return false;
+    })
+
   })
 }

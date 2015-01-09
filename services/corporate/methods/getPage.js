49b371d3da587ccdd63288707d25972005bda0c5
Zeke Sikelianos <zeke@sikelianos.com>
2015-01-08 17:58:21 -0800

    sanitize static pages with marky-markdown

--- services/corporate/methods/getPage.js
+++ services/corporate/methods/getPage.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var Joi = require('joi');
-var marked = require('marked');
+var marky = require('marky-markdown');
 var fmt = require('util').format;
 
 module.exports = {
@@ -26,7 +26,7 @@ function getPage (repo) {
           return next(err, null);
         }
 
-        return next(err, marked.parse(content));
+        return next(err, marky(content).html());
       });
     });
   };

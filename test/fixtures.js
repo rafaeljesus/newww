9cfcffaa9af8476731cf666f3ade5bfcc344e90c
Zeke Sikelianos <zeke@sikelianos.com>
2015-02-09 14:28:06 -0800

    introduce master fixture

---
+++ test/fixtures.js
@@ -0,0 +1,11 @@
+module.exports = {
+  browse: require("./fixtures/browse/index"),
+  customers: require("./fixtures/customers/index"),
+  email_edit: require("./fixtures/email_edit"),
+  enterprise: require("./fixtures/enterprise"),
+  packages: require("./fixtures/packages/index"),
+  policies: require("./fixtures/policies"),
+  search: require("./fixtures/search"),
+  signup: require("./fixtures/signup"),
+  users: require("./fixtures/users"),
+}
---
+++ test/fixtures/packages/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  benchmark: require("./benchmark.json"),
+  fake: require("./fake.json"),
+  request: require("./request.json"),
+  unpublished: require("./unpublished.json"),
+}

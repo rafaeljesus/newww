b4ca2302a97a0eef88212cb1642b10637205a0f3
Zeke Sikelianos <zeke@sikelianos.com>
2015-02-27 16:11:18 -0800

    alternative collaborator client usage in the handler

--- handlers/collaborator.js
+++ handlers/collaborator.js
@@ -1,49 +1,54 @@
 var collaborator = module.exports = {};
+var Collaborator = require("../models/collaborator")
 
-collaborator.list = function (request, reply) {
-  var Collaborator = require("../models/collaborator").new(request)
-
-  Collaborator.list(request.params.package, function(err, collaborators) {
+collaborator.list = function(request, reply) {
+  Collaborator.new(request)
+  .list(request.params.package, function(err, collaborators) {
     if (err) {
       request.logger.error(err);
       return reply(err)
     }
-    return reply({collaborators: collaborators});
+    return reply({
+      collaborators: collaborators
+    });
   });
 };
 
-collaborator.add = function (request, reply) {
-  var Collaborator = require("../models/collaborator").new(request)
-
-  Collaborator.add(request.params.package, request.payload.collaborator, function(err, collaborator) {
+collaborator.add = function(request, reply) {
+  Collaborator.new(request)
+  .add(request.params.package, request.payload.collaborator, function(err, collaborator) {
     if (err) {
       request.logger.error(err);
       return reply(err)
     }
-    return reply({collaborator: collaborator});
+    return reply({
+      collaborator: collaborator
+    });
   });
 };
 
-collaborator.update = function (request, reply) {
-  var Collaborator = require("../models/collaborator").new(request)
-
-  Collaborator.update(request.params.package, request.payload.collaborator, function(err, collaborator) {
+collaborator.update = function(request, reply) {
+  Collaborator.new(request)
+  .update(request.params.package, request.payload.collaborator, function(err, collaborator) {
     if (err) {
       request.logger.error(err);
       return reply(err)
     }
-    return reply({collaborator: collaborator});
+    return reply({
+      collaborator: collaborator
+    });
   });
 };
 
-collaborator.del = function (request, reply) {
-  var Collaborator = require("../models/collaborator").new(request)
-
-  Collaborator.del(request.params.package, request.params.username, function(err, collaborator) {
+collaborator.del = function(request, reply) {
+  Collaborator.new(request)
+  .del(request.params.package, request.params.username, function(err, collaborator) {
     if (err) {
       request.logger.error(err);
       return reply(err)
     }
-    return reply({collaborator: collaborator});
+    return reply({
+      collaborator: collaborator
+    });
   });
 };

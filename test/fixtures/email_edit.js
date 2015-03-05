ab938089307fdf6b7267a9f65ac3faf2ca5efb28
rockbot <raquel@rckbt.me>
2015-03-04 16:17:26 -0800

    fixes /email-edit

--- test/fixtures/email_edit.js
+++ test/fixtures/email_edit.js
@@ -1,30 +1,26 @@
-var bob = require('./users').fakeusercouch,
-    newEmail = 'new@fakeuser.com';
+var bob = require('./users').bob,
+    newEmail = 'new@boom.me';
 
 module.exports = function (cookieCrumb) {
   return {
     newEmail: {
-      _id: bob._id,
       name: bob.name,
       password: '12345',
       email: newEmail,
       crumb: cookieCrumb
     },
     missingEmail: {
-      _id: bob._id,
       name: bob.name,
       password: '12345',
       crumb: cookieCrumb
     },
     invalidEmail: {
-      _id: bob._id,
       name: bob.name,
       password: '12345',
       email: 'blarg@boom',
       crumb: cookieCrumb
     },
     invalidPassword: {
-      _id: bob._id,
       name: bob.name,
       password: 'password',
       email: newEmail,

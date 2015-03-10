e3916a7a5cacdc13b438af7c366f872bcd1debd2
Zeke Sikelianos <zeke@sikelianos.com>
2015-03-09 17:04:01 -0700

    update package access handler to use the real package client

--- handlers/access.js
+++ handlers/access.js
@@ -1,22 +1,20 @@
-var http = require("request");
 var _ = require("lodash");
-var utils = require("../lib/utils");
 
 module.exports = function(request, reply) {
   var Collaborator = require("../models/collaborator").new(request)
-  var ctx = {};
+  var Package = require("../models/package").new(request)
+  var context = {}
+  var package
 
-  http("http://registry.npmjs.org/" + request.params.package, function(err, res, body) {
-
-    var parsedBody = utils.safeJsonParse(body);
-
-    var package = _.pick(parsedBody, ["name", "description", "dist-tags"]);
+  Package.get(request.params.package)
+  .then(function(pkg){
+    package = _.pick(pkg, ["name", "description", "dist-tags"]);
     Collaborator.list(package.name)
       .then(function(collaborators) {
         package.collaborators = collaborators;
-        ctx.package = package;
-        return reply.view('package/access', ctx);
-      });
-  });
+        context.package = package;
+        return reply.view('package/access', context);
+      })
+  })
 
 };

6d4d498210daacb6bf066cf68eeb61084f5204ec
Zeke Sikelianos <zeke@sikelianos.com>
2015-03-11 13:58:50 -0700

    clean up the access handler now that all the tests are in place

--- handlers/access.js
+++ handlers/access.js
@@ -1,7 +1,9 @@
-var _ = require("lodash");
+var pick = require("lodash").pick;
 
 module.exports = function(request, reply) {
-  var user = request.auth.credentials;
+  var package
+  var context = {}
+  var user = request.auth.credentials
   var Collaborator = require("../models/collaborator").new(request)
   var Package = require("../models/package").new(request)
   var desiredPackageFields = [
@@ -10,38 +12,30 @@ module.exports = function(request, reply) {
     "scoped",
     "private"
   ];
-  var context = {}
-  var package
 
   Package.get(request.packageName)
   .then(function(pkg){
+    package = pick(pkg, desiredPackageFields);
+    return Collaborator.list(package.name)
+  })
+  .then(function(collaborators) {
+    package.collaborators = collaborators;
 
-    package = _.pick(pkg, desiredPackageFields);
-    Collaborator.list(package.name)
-      .then(function(collaborators) {
+    if (user && user.name in package.collaborators) {
+      user.hasReadAccessToPackage = true
+      user.hasWriteAccessToPackage = package.collaborators[user.name].write
+    }
 
-        Object.keys(collaborators).forEach(function(key){
-          collaborators[key].write = collaborators[key].permissions === "write"
-        })
+    if (package.private && (!user || !user.hasReadAccessToPackage)) {
+      return reply.view('errors/not-found').code(404);
+    }
 
-        package.collaborators = collaborators;
+    context.enablePermissionTogglers = package.scoped
+      && user
+      && user.hasWriteAccessToPackage
 
-        if (user && user.name in package.collaborators) {
-          user.hasReadAccessToPackage = true
-          user.hasWriteAccessToPackage = package.collaborators[user.name].permissions === "write"
-        }
-
-        if (package.private && (!user || !user.hasReadAccessToPackage)) {
-          return reply.view('errors/not-found').code(404);
-        }
-
-        context.enablePermissionTogglers = package.scoped
-          && user
-          && user.hasWriteAccessToPackage
-
-        context.package = package
-        return reply.view('package/access', context)
-      })
+    context.package = package
+    return reply.view('package/access', context)
   })
 
 };

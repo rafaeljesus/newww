9d1d9e6cf0985c5af96dda28901ac9c10ae6af36
Zeke Sikelianos <zeke@sikelianos.com>
2015-03-12 23:49:40 -0700

    tweak the client-side access handler

--- assets/scripts/package-access.js
+++ assets/scripts/package-access.js
@@ -1,76 +1,80 @@
-var templates = {
-  collaborator: require("../../templates/partials/collaborator.hbs"),
-}
+var template = require("../../templates/partials/collaborator.hbs");
 
 module.exports = function() {
   $(function(){
-    enableAdminControls()
-    $('#add-collaborator').submit(addCollaborator)
-
-    $('.remove-collaborator a').click(function(e){
-      $(this).parents('form').submit()
-    })
-    $('.remove-collaborator').submit(removeCollaborator)
+    updateInputsAndHandlers()
   })
 }
 
 var addCollaborator = function(e) {
   e.preventDefault()
-
-  var collaborator = {}
-  $(this).serializeArray().forEach(function(input){
-    collaborator[input.name] = input.value;
-  })
-
-  var opts = {
-    method: $(this).attr("method"),
-    url: $(this).attr("action"),
-    data: {collaborator: collaborator},
-    headers: {'x-csrf-token': window.crumb},
-    json: true
-  }
+  var opts = formToRequestOptions($(this))
+  opts.data = {collaborator: formToObject($(this))}
 
   $.ajax(opts)
     .done(function(data){
-      // console.log(data, templates.collaborator(data.collaborator))
-      $("tr.collaborator:last").after(templates.collaborator(data.collaborator))
-      enableAdminControls()
+      $("tr.collaborator:last").after(template(data.collaborator))
     })
-    .error(function(){
-      console.error(arguments)
-    })
+    .done(successHandler)
+    .fail(errorHandler)
+}
+
+var updateCollaborator = function(e) {
+  e.preventDefault()
+  var $form = $(this).parents("form")
+  var opts = formToRequestOptions($form)
+  opts.data = {
+    collaborator: formToObject($form),
+    crumb: window.crumb
+  }
+  $.ajax(opts)
+    .done(successHandler)
+    .fail(errorHandler)
 }
 
 var removeCollaborator = function(e) {
   e.preventDefault()
+  var opts = formToRequestOptions($(this))
+  $.ajax(opts)
+    .done(function(data){
+      $("#collaborator-"+data.username).slideUp()
+    })
+    .done(successHandler)
+    .fail(errorHandler)
+}
 
-  var opts = {
-    method: $(this).attr("method"),
-    url: $(this).attr("action"),
+var updateInputsAndHandlers = function() {
+  $('#add-collaborator').submit(addCollaborator)
+  $('.update-collaborator input').change(updateCollaborator)
+  $('.remove-collaborator').submit(removeCollaborator)
+  $("[data-enable-permission-togglers='true'] input").attr('disabled', false)
+  $("[data-enable-deletion='true'] form.remove-collaborator").css({display: "block"})
+  $("#add-collaborator input[name='name']").val("")
+}
+
+var formToRequestOptions = function($el) {
+  return {
+    method: $el.attr("method"),
+    url: $el.attr("action"),
     headers: {'x-csrf-token': window.crumb},
     json: true
   }
-
-  $.ajax(opts)
-    .done(function(data){
-      console.log(data)
-      $("#collaborator-"+data.username).slideUp()
-      enableAdminControls()
-    })
-    .error(function(){
-      console.error(arguments)
-    })
 }
 
-var enableAdminControls = function() {
-
-  // enable admin controls
-  $("[data-enable-permission-togglers='true'] input")
-    .attr('disabled', false)
-
-  $("[data-enable-deletion='true'] form.remove-collaborator")
-    .css({display: "block"})
-
-  // empty the collaborator input
-  $("#add-collaborator input[name='name']").val("")
+var formToObject = function($el) {
+  var obj = {}
+  $el.serializeArray().forEach(function(input){
+    obj[input.name] = input.value
+  })
+  return obj
 }
+
+ var errorHandler = function(xhr, status, error) {
+   console.error(error)
+   $("p.error").text(error).show()
+ }
+
+ var successHandler = function(data) {
+   console.log(data)
+   updateInputsAndHandlers()
+ }

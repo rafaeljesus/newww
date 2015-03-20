44bd976751a1013bf8c9f53342e18dbfb48ade95
Zeke Sikelianos <zeke@sikelianos.com>
2015-03-19 21:24:39 -0700

    lowercase some of the things

--- locales/en_US/user.js
+++ locales/en_US/user.js
@@ -1,49 +1,49 @@
 module.exports = {
   signup: {
-    title: "Create an Account",
-    subtitle: "Sign all the way up",
+    title: "create an account",
+    subtitle: "sign all the way up",
     error: "There was a problem! The server said:",
     form: {
-      username: "Username",
-      nameHelp: "Must be lowercase and composed of URL-safe characters.",
-      password: "Password",
-      verify: "Verify Password",
-      email: "Email Address",
+      username: "username",
+      nameHelp: "must be lowercase and composed of URL-safe characters.",
+      password: "password",
+      verify: "verify password",
+      email: "email address",
       emailHelp: "This <strong>will</strong> be public and shown on all the packages you publish. Accounts with invalid emails may be deleted without notice.",
-      npmWeekly: "Sign up for npm Weekly emails",
-      submit: "Make it so"
+      npmWeekly: "sign up for npm weekly emails",
+      submit: "make it so"
     }
   },
 
   login: {
-    title: "Login",
-    subtitle: "You look lovely today, btw.",
+    title: "login",
+    subtitle: "you look lovely today, btw.",
     error: {
-      invalid: "Invalid username or password",
+      invalid: "invalid username or password",
       invalidContext: "If you feel that this is a mistake, please email <a href='mailto:support@npmjs.com'>support@npmjs.com</a> with the following unique error ID:"
     },
     form: {
-      username: "Username",
-      password: "Password",
-      forgot: "Forgot your password?",
-      signup: "Create an Account",
-      submit: "Login"
+      username: "username",
+      password: "password",
+      forgot: "forgot your password?",
+      signup: "create an account",
+      submit: "login"
     }
   },
 
   password: {
-    title: "Forgot your password?",
-    subtitle: "Don't worry. It happens to the best of us.",
+    title: "forgot your password?",
+    subtitle: "don't worry. it happens to the best of us.",
     emailed: "An email has been sent. Click the link when you get it.",
     multipleUsernames: "Found multiple usernames for the given email address. Please choose one.",
     form: {
-      submit: "Email me a recovery link"
+      submit: "email me a recovery link"
     }
   },
 
   profile: {
     not_found: {
-      subtitle: "Sorry, there's no user by that name."
+      subtitle: "sorry, there's no user by that name."
     }
   }
 

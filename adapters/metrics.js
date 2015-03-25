3c9d66dc6734f99c7864409bf34c6bca46c54368
Zeke Sikelianos <zeke@sikelianos.com>
2015-03-25 09:56:50 -0700

    allow METRICS_HOST env var, but default to udp://localhost:3333

--- adapters/metrics.js
+++ adapters/metrics.js
@@ -9,7 +9,7 @@ var metrics = module.exports = function constructEmitter()
     if (emitter) return emitter;
 
     emitter = new Emitter({
-        uri: 'udp://localhost:3333',
+        uri: process.env.METRICS_URL || 'udp://localhost:3333',
         node: os.hostname()
     });
     return emitter;

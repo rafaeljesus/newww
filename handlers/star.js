df58fedb1105378ebe5b9e3d304e71fb666ea6bd
C J Silverio <ceej@npmjs.com>
2015-04-01 16:37:24 -0700

    Promise.catch(err).then() considered incorrect.
    The correct order of operations here is
    `Promise.then(normal-flow).catch(error);`
    If you catch an error, return from that function, then pass the
    result along to a then(), whatever's in the then will be executed.
    
    Caught this because a misconfigured service was causing all star
    operations to error, which then caused a double-reply error in www.

--- handlers/star.js
+++ handlers/star.js
@@ -21,32 +21,31 @@ module.exports = function (request, reply) {
 
   if (starIt) {
     Package.star(pkg)
+      .then(function () {
+        request.timing.page = 'star';
+        request.metrics.metric({ name: 'star', package: pkg });
+        return reply(username + ' starred ' + pkg).code(200);
+      })
       .catch(function (err) {
         request.logger.error(username + ' was unable to star ' + pkg);
         request.logger.error(err);
         reply('not ok').code(500);
         return;
-      })
-      .then(function () {
-        request.timing.page = 'star';
-        request.metrics.metric({ name: 'star', package: pkg });
-        return reply(username + ' starred ' + pkg).code(200);
       });
-
   } else {
 
     Package.unstar(pkg)
-      .catch(function (err) {
-        request.logger.error(username + ' was unable to unstar ' + pkg);
-        request.logger.error(err);
-        reply('not ok').code(500);
-        return;
-      })
       .then(function () {
         request.timing.page = 'unstar';
         request.metrics.metric({ name: 'unstar', package: pkg });
 
         return reply(username + ' unstarred ' + pkg).code(200);
+      })
+      .catch(function (err) {
+        request.logger.error(username + ' was unable to unstar ' + pkg);
+        request.logger.error(err);
+        reply('not ok').code(500);
+        return;
       });
   }
 };

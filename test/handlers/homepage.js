c2e8c3d13d5e520ae678f464fab030f62919fedc
Zeke Sikelianos <zeke@sikelianos.com>
2015-04-07 16:30:29 -0700

    add some tests around logged-in and logged-out #user-nav

--- test/handlers/homepage.js
+++ test/handlers/homepage.js
@@ -22,7 +22,8 @@ after(function (done) {
   server.stop(done);
 });
 
-describe('GET /', function () {
+describe('GET / for an anonymous user', function () {
+  var $
   var resp
   var options = {url: "/"}
   var packageMock = nock("https://user-api-example.com")
@@ -43,6 +44,7 @@ describe('GET /', function () {
   before(function(done){
     server.inject(options, function (response) {
       resp = response
+      $ = cheerio.load(resp.result)
       packageMock.done()
       downloadsMock.done()
       done()
@@ -71,6 +73,61 @@ describe('GET /', function () {
     expect(context.totalPackages).to.be.a.number();
     var $ = cheerio.load(resp.result)
     expect($(".total-packages").text()).to.equal(String(context.totalPackages));
+    done();
+  });
+
+  it("displays a wombat login link", function (done) {
+    expect($("#user-info a[href='/login'] img[src*='wombat']").length).to.equal(1);
+    done();
+  });
+
+});
+
+
+describe('GET / for a logged-in user', function () {
+  var $
+  var resp
+  var options = {
+    url: "/",
+    credentials: fixtures.users.mikeal
+  }
+
+  var userMock = nock('https://user-api-example.com')
+    .get('/user/mikeal')
+    .reply(200, fixtures.users.mikeal);
+  var packageMock = nock("https://user-api-example.com")
+    .get('/package?sort=dependents')
+    .reply(200, fixtures.aggregates.most_depended_upon_packages)
+    .get('/package?sort=modified')
+    .reply(200, fixtures.aggregates.recently_updated_packages)
+    .get('/package/-/count')
+    .reply(200, 12345);
+  var downloadsMock = nock("https://downloads-api-example.com")
+    .get('/point/last-day')
+    .reply(200, fixtures.downloads.all.day)
+    .get('/point/last-week')
+    .reply(200, fixtures.downloads.all.week)
+    .get('/point/last-month')
+    .reply(200, fixtures.downloads.all.month);
+
+  before(function(done){
+    server.inject(options, function (response) {
+      resp = response
+      $ = cheerio.load(resp.result)
+      userMock.done()
+      packageMock.done()
+      downloadsMock.done()
+      done()
+    })
+  })
+
+  it("displays an avatar linking to the user's profile page", function (done) {
+    expect($("#user-info a[href='/~mikeal'] img[src^='https://secure.gravatar']").length).to.equal(1);
+    done();
+  });
+
+  it("displays a link to log out", function (done) {
+    expect($("#user-info a[href='/logout']").length).to.equal(1);
     done();
   });
 

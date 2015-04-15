05e4aa8a07ddfad0dd444e696af906df870fe3a1
Zeke Sikelianos <zeke@sikelianos.com>
2015-04-15 16:36:11 -0700

    don’t show banner on the billing page

--- assets/scripts/banner.js
+++ assets/scripts/banner.js
@@ -3,7 +3,12 @@ module.exports = function() {
     var ready = new Date() > new Date("2015-04-14T03:30:00-07:00")
     var disabled = localStorage.getItem('disable-private-modules-banner')
 
-    if (!ready || disabled || location.pathname === "/private-modules") {
+    if (
+      !ready
+      || disabled
+      || location.pathname === "/private-modules"
+      || location.pathname === "/settings/billing"
+    ) {
       return
     }
 

6ba7337e04091ac0c04a93c3b59256ec09a9a164
Zeke Sikelianos <zeke@sikelianos.com>
2015-04-22 18:27:47 -0700

    don't bother passsing options with the twitter pixel id

--- assets/scripts/twitter-tracking.js
+++ assets/scripts/twitter-tracking.js
@@ -2,10 +2,7 @@ $(function(){
   if (typeof twttr === "function") {
     var pid = $("[data-twitter-pid]").data('twitterPid');
     if (pid) {
-      twttr.conversion.trackPid(pid, {
-        tw_sale_amount: 0,
-        tw_order_quantity: 0
-      });
+      twttr.conversion.trackPid(pid);
     }
   }
 });

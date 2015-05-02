b6b4411091193278e1a34b5f835b1d45d59122f8
Zeke Sikelianos <zeke@sikelianos.com>
2015-05-01 20:37:49 -0700

    remove old commented-out mocks

--- test/handlers/user/email-edit.js
+++ test/handlers/user/email-edit.js
@@ -32,18 +32,6 @@ var postEmail = function (emailOpts) {
 };
 
 before(function (done) {
-  // nock.cleanAll();
-  // nock("https://user-api-example.com")
-  //   .get('/user/' + users.bob.name).times(11)
-  //   .reply(200, fixtures.users.bob)
-  //   .get('/user/' + users.mikeal.name)
-  //   .reply(200, fixtures.users.mikeal)
-  //   .get('/user/noone')
-  //   .reply(200, {
-  //     name: "noone",
-  //     email: "f@boom.me",
-  //   });
-
   require('../../mocks/server')(function (obj) {
     server = obj;
     done();

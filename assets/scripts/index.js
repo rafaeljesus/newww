c0faf1538bcd34e460760c4aaf284d8a5bade245
Zeke Sikelianos <zeke@sikelianos.com>
2015-05-04 14:18:10 -0700

    add twitter tracking to assets/scripts/index.js and fix jshint formatting

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,24 +1,25 @@
-window.$ = require("jquery")
+window.$ = require("jquery");
 
-require("./crumb")()
-require("./hiring")
-require("./npm-expansions")
-require("./email-obfuscate")()
-require("./payments")()
-require("./pretty-numbers")()
-require("./private-npm-beta")()
-require('./deep-links')()
-require("./tooltips")()
-require("./what-npm-is-for")()
-require("./billing")()
-require("./billing-cancel")()
-require("./banner")()
-require("./date-formatting")()
-require("./keyboard-shortcuts")()
-require("./add-active-class-to-links")()
-require("./autoselect-inputs")()
-require("./package-access")()
-require("./buy-enterprise-license")()
+require("./crumb")();
+require("./hiring");
+require("./npm-expansions");
+require("./email-obfuscate")();
+require("./payments")();
+require("./pretty-numbers")();
+require("./private-npm-beta")();
+require('./deep-links')();
+require("./tooltips")();
+require("./what-npm-is-for")();
+require("./billing")();
+require("./billing-cancel")();
+require("./banner")();
+require("./date-formatting")();
+require("./keyboard-shortcuts")();
+require("./add-active-class-to-links")();
+require("./autoselect-inputs")();
+require("./package-access")();
+require("./buy-enterprise-license")();
+require("./twitter-tracking")();
 
-window.github = require("./github")()
-window.star = require("./star")()
+window.github = require("./github")();
+window.star = require("./star")();

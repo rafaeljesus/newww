a699974c31d4774c309d44e46ae0d290a4fb279d
Zeke Sikelianos <zeke@sikelianos.com>
2015-05-04 13:15:32 -0700

    log twitter conversions to the console

--- assets/scripts/twitter-tracking.js
+++ assets/scripts/twitter-tracking.js
@@ -2,6 +2,7 @@ $(function(){
   if (typeof twttr === "function") {
     var pid = $("[data-twitter-pid]").data('twitterPid');
     if (pid) {
+      console.log("tracking twitter conversion: " + pid);
       twttr.conversion.trackPid(pid);
     }
   }

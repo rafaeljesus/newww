3f5c8d8ac9ac83a5c5ebbec2bd8dfe3d26319f5b
Zeke Sikelianos <zeke@sikelianos.com>
2015-05-04 13:42:45 -0700

    annotate twitter codes

--- assets/scripts/twitter-tracking.js
+++ assets/scripts/twitter-tracking.js
@@ -1,3 +1,9 @@
+/*
+  l5xz2: billing page viewed
+  l5xyy: private npm purchased
+  l5ybn: email verified
+*/
+
 $(function(){
   if (typeof twttr === "function") {
     var pid = $("[data-twitter-pid]").data('twitterPid');

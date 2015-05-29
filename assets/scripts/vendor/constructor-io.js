85fec4d6b6471f4d7184ed8971fd896b62c8717d
rockbot <raquel@rckbt.me>
2015-05-29 11:20:49 -0700

    adds more styling

--- assets/scripts/vendor/constructor-io.js
+++ assets/scripts/vendor/constructor-io.js
@@ -1,6 +1,14 @@
 $(function() {
+
+  var searchWidth = $('#npm-search').outerWidth() - $('#site-search-submit').outerWidth();
+
   $.getScript("//cnstrc.com/js/ac.js", function() {
-    $('#site-search').constructorAutocomplete({ key: 'CD06z4gVeqSXRiDL2ZNK', directResults: true, maxHeight: 400 });
-  })
+    $('#site-search').constructorAutocomplete({ key: 'CD06z4gVeqSXRiDL2ZNK', directResults: true, maxHeight: 400, width: searchWidth });
+  });
+
+  $(window).resize(function () {
+    searchWidth = $('#npm-search').outerWidth() - $('#site-search-submit').outerWidth();
+    $('.autocomplete-suggestions').outerWidth(searchWidth);
+  });
 });
 

17d1f4b4fed07f5a0025041d68e9bfaf661aa495
Laurie Voss <github@seldo.com>
2015-06-09 22:15:40 -0700

    Update whoshiring email address to .com

--- locales/en_US/company.js
+++ locales/en_US/company.js
@@ -2,6 +2,6 @@ module.exports = {
   whoshiring: {
     title: "Who's Hiring?",
     subtitle: "These companies are looking for JavaScript and Node.js developers",
-    addCompany: "To add your company to this list, email <a href='mailto:whoshiring@npmjs.org'>whoshiring@npmjs.org</a> for pricing options."
+    addCompany: "To add your company to this list, email <a href='mailto:whoshiring@npmjs.com'>whoshiring@npmjs.com</a> for pricing options."
   }
 }

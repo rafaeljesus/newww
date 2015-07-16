586761a1cbb29b4551f3b6a292ba8793d1b8203e
Jeffrey Lembeck <jlembeck@gmail.com>
2015-07-16 11:19:34 -0700

    formatting

--- handlers/org.js
+++ handlers/org.js
@@ -4,7 +4,8 @@ var exp = {};
 
 exp.getOrg = function (request, reply) {
   var opts = {};
-  Org(request.loggedInUser.name).get(request.params.org, function (err, org) {
+  Org(request.loggedInUser.name)
+    .get(request.params.org, function (err, org) {
     if (err) { request.logger.error(err); }
     opts.org = org;
     reply.view('org/info', opts);

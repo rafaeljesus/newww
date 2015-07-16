000a4635c405f9c2fbc64a97a09e04df0ec3802d
Jeffrey Lembeck <jlembeck@gmail.com>
2015-07-16 11:18:41 -0700

    Add index page for viewing all orgs

--- facets/user/show-orgs.js
+++ facets/user/show-orgs.js
@@ -1,14 +1,14 @@
 var User = require('../../models/user');
 
 module.exports = function(request, reply) {
-  var name = request.params.name;
+  var name = request.loggedInUser.name;
   var opts = {};
 
   User.new(request)
     .getOrgs(name)
     .then(function (orgs) {
       opts.orgs = orgs;
-      return reply.view('org/info', opts);
+      return reply.view('org/index', opts);
     })
     .catch(function (err) {
       return reply.view('errors/interal', opts);

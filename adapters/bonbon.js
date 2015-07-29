8c2dd37c447312ee1a976271dd70433df6d8bfbc
C J Silverio <ceejceej@gmail.com>
2015-07-29 16:19:26 -0700

    A better fix: don't create a user object if we don't have one.

--- adapters/bonbon.js
+++ adapters/bonbon.js
@@ -39,8 +39,7 @@ exports.register = function(server, options, next) {
     if (request.auth && request.auth.credentials && !request.path.match(/static\//)) {
       UserModel.new(request).get(request.auth.credentials.name, function(err, user) {
         if (err) { request.logger.warn(err); }
-        user = user || {};
-        user.sid = request.auth.credentials.sid;
+        if (user) { user.sid = request.auth.credentials.sid; }
         request.loggedInUser = user;
         request.customer = user && new CustomerModel(user.name);
         completePreHandler();

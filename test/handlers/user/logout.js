af4cc21b5c2fd2f16611bf1019b81dd87b49e1e3
rockbot <raquel@rckbt.me>
2015-08-03 15:32:51 -0400

    adds tests for logout user handler

---
+++ test/handlers/user/logout.js
@@ -0,0 +1,54 @@
+var Code = require('code'),
+    Lab = require('lab'),
+    lab = exports.lab = Lab.script(),
+    describe = lab.experiment,
+    before = lab.before,
+    after = lab.after,
+    it = lab.test,
+    expect = Code.expect,
+    redis = require("../../../adapters/redis-sessions"),
+    server,
+    sinon = require('sinon'),
+    users = require('../../fixtures').users;
+
+before(function (done) {
+  require('../../mocks/server')(function (obj) {
+    server = obj;
+    server.methods.user.delSession = sinon.spy(server.methods.user.delSession);
+    done();
+  });
+});
+
+after(function (done) {
+  server.methods.user.delSession.reset();
+  redis.flushdb();
+  server.stop(done);
+});
+
+describe('logout', function () {
+  it('redirects to the homepage if there is no logged in user', function (done) {
+    var options = {
+      url: '/logout'
+    };
+
+    server.inject(options, function (resp) {
+      expect(resp.statusCode).to.equal(302);
+      expect(resp.request.response.headers.location).to.equal('/');
+      done();
+    });
+  });
+
+  it('deletes the session for the logged in user', function (done) {
+    var options = {
+      url: '/logout',
+      credentials: users.bob
+    };
+
+    server.inject(options, function (resp) {
+      expect(resp.statusCode).to.equal(302);
+      expect(server.methods.user.delSession.called).to.be.true();
+      expect(resp.request.response.headers.location).to.equal('/');
+      done();
+    });
+  });
+});

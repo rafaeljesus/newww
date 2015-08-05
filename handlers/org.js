f0e3e7e5c6a8a561f07738ad569bf3e43bb8054e
rockbot <raquel@rckbt.me>
2015-08-05 16:26:50 -0700

    divides updateOrg into addUserToOrg and removeUserToOrg

--- handlers/org.js
+++ handlers/org.js
@@ -33,7 +33,7 @@ exports.getOrg = function (request, reply) {
   });
 };
 
-exports.updateOrg = function (request, reply) {
+exports.addUserToOrg = function (request, reply) {
   var orgName = request.params.org;
   var loggedInUser = request.loggedInUser.name;
   var user = {
@@ -42,75 +42,91 @@ exports.updateOrg = function (request, reply) {
   };
   var opts = {};
 
-  if (request.payload.updateType === "addUser") {
-    Org(loggedInUser)
-      .addUser(orgName, user, function (err, addedUser) {
+  Org(loggedInUser)
+    .addUser(orgName, user, function (err, addedUser) {
+      if (err) {
+        request.logger.error(err);
+        return reply.view('errors/internal', err).code(err.statusCode);
+      }
+      request.customer.getLicenseIdForOrg(orgName, function(err, licenseId) {
         if (err) {
           request.logger.error(err);
-          return reply.view('errors/internal', err).code(err.statusCode);
+          return reply.view('errors/internal', err).code(404);
         }
-        request.customer.getLicenseIdForOrg(orgName, function(err, licenseId) {
+        request.customer.extendSponsorship(licenseId, user.user, function(err, extendedSponsorship) {
           if (err) {
             request.logger.error(err);
-            return reply.view('errors/internal', err).code(404);
+            return reply.view('errors/internal', err).code(err.statusCode);
           }
-          request.customer.extendSponsorship(licenseId, user.user, function(err, extendedSponsorship) {
-            if (err) {
-              request.logger.error(err);
-              return reply.view('errors/internal', err).code(err.statusCode);
-            }
-            request.customer.acceptSponsorship(extendedSponsorship.verification_key, function(err) {
-              if (err) {
-                request.logger.error(err);
-                return reply.view('errors/internal', err).code(err.statusCode);
-              }
-              Org(loggedInUser)
-                .get(orgName, function (err, org) {
-                  if (err) {
-                    request.logger.error(err);
-                    return reply.view('errors/internal', err);
-                  }
-                  opts.org = org;
-                  return reply.view('org/info', opts);
-                });
-            });
-          });
-        });
-      });
-  } else if (request.payload.updateType === "deleteUser") {
-    request.customer.getLicenseIdForOrg(orgName, function(err, licenseId) {
-
-      if (err) {
-        request.logger.error('could not get license ID for ' + orgName);
-        request.logger.error(err);
-        // TODO: make better error page here
-        return reply.view('errors/internal', err).code(404);
-      }
-
-      request.customer.revokeSponsorship(user.user, licenseId, function (err) {
-
-        if (err) {
-          request.logger.error('issue revoking sponsorship for user ', user);
-          request.logger.error(err);
-          // TODO: make better error page here
-          return reply.view('errors/internal', err).code(err.statusCode);
-        }
-
-        Org(loggedInUser)
-          .removeUser(orgName, user.user, function (err) {
+          request.customer.acceptSponsorship(extendedSponsorship.verification_key, function(err) {
             if (err) {
               request.logger.error(err);
               return reply.view('errors/internal', err).code(err.statusCode);
             }
             Org(loggedInUser)
               .get(orgName, function (err, org) {
-                if (err) { request.logger.error(err); }
+                if (err) {
+                  request.logger.error(err);
+                  return reply.view('errors/internal', err);
+                }
                 opts.org = org;
                 return reply.view('org/info', opts);
               });
           });
+        });
       });
     });
+};
+
+exports.removeUserFromOrg = function (request, reply) {
+  var orgName = request.params.org;
+  var loggedInUser = request.loggedInUser.name;
+  var user = {
+    user: request.payload.username,
+    role: request.payload.role
+  };
+  var opts = {};
+
+  request.customer.getLicenseIdForOrg(orgName, function(err, licenseId) {
+
+    if (err) {
+      request.logger.error('could not get license ID for ' + orgName);
+      request.logger.error(err);
+      // TODO: make better error page here
+      return reply.view('errors/internal', err).code(404);
+    }
+
+    request.customer.revokeSponsorship(user.user, licenseId, function (err) {
+
+      if (err) {
+        request.logger.error('issue revoking sponsorship for user ', user);
+        request.logger.error(err);
+        // TODO: make better error page here
+        return reply.view('errors/internal', err).code(err.statusCode);
+      }
+
+      Org(loggedInUser)
+        .removeUser(orgName, user.user, function (err) {
+          if (err) {
+            request.logger.error(err);
+            return reply.view('errors/internal', err).code(err.statusCode);
+          }
+          Org(loggedInUser)
+            .get(orgName, function (err, org) {
+              if (err) { request.logger.error(err); }
+              opts.org = org;
+              return reply.view('org/info', opts);
+            });
+        });
+    });
+  });
+};
+
+exports.updateOrg = function (request, reply) {
+  if (request.payload.updateType === "addUser") {
+    exports.addUserToOrg(request, reply);
+  } else if (request.payload.updateType === "deleteUser") {
+    exports.removeUserFromOrg(request, reply);
   }
 };
 

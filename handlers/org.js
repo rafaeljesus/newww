d2f8e153532ff86bb8895b6a3cfe72083ca706c1
rockbot <raquel@rckbt.me>
2015-08-05 11:43:23 -0700

    moves updateOrg from customer handler to org handler

--- handlers/org.js
+++ handlers/org.js
@@ -1,9 +1,8 @@
 var Org = require('../agents/org');
 var Customer = require('../models/customer');
-var exp = {};
+var Org = {};
 
-
-exp.getOrg = function (request, reply) {
+Org.getOrg = function (request, reply) {
   var opts = {};
   var loggedInUser = request.loggedInUser && request.loggedInUser.name;
   Org(loggedInUser)
@@ -35,7 +34,90 @@ exp.getOrg = function (request, reply) {
   });
 };
 
-exp.deleteOrg = function (request, reply) {
+Org.updateOrg = function (request, reply) {
+  var orgName = request.params.org;
+  var loggedInUser = request.loggedInUser.name;
+  var user = {
+    user: request.payload.username,
+    role: request.payload.role
+  };
+  var opts = {};
+
+  if (request.payload.updateType === "addUser") {
+    Org(loggedInUser)
+      .addUser(orgName, user, function (err, addedUser) {
+        if (err) {
+          request.logger.error(err);
+          return reply.view('errors/internal', err);
+        }
+        request.customer.getLicenseIdForOrg(orgName, function(err, licenseId) {
+          if (err) {
+            request.logger.error(err);
+            return reply.view('errors/internal', err);
+          }
+          request.customer.extendSponsorship(licenseId, user.user, function(err, extendedSponsorship) {
+            if (err) {
+              request.logger.error(err);
+              return reply.view('errors/internal', err);
+            }
+            request.customer.acceptSponsorship(extendedSponsorship.verification_key, function(err) {
+              if (err) {
+                request.logger.error(err);
+                return reply.view('errors/internal', err);
+              }
+              Org(loggedInUser)
+                .get(orgName, function (err, org) {
+                  if (err) {
+                    request.logger.error(err);
+                    return reply.view('errors/internal', err);
+                  }
+                  opts.org = org;
+                  return reply.view('org/info', opts);
+                });
+            });
+          });
+        });
+      });
+  } else if (request.payload.updateType === "deleteUser") {
+    request.customer.getLicenseIdForOrg(orgName, function(err, licenseId) {
+
+      if (err) {
+        request.logger.error('could not get license ID for ' + orgName);
+        request.logger.error(err);
+        // TODO: make better error page here
+        return reply.view('errors/internal', err);
+      }
+
+      request.customer.revokeSponsorship(user.user, licenseId, function (err) {
+
+        if (err) {
+          request.logger.error('issue revoking sponsorship for user ' + user);
+          request.logger.error(err);
+          // TODO: make better error page here
+          return reply.view('errors/internal', err);
+        }
+
+        Org(loggedInUser)
+          .removeUser(orgName, user.user, function (err) {
+            if (err) {
+              request.logger.error(err);
+              return reply.view('errors/internal', err);
+            }
+            Org(loggedInUser)
+              .get(orgName, function (err, org) {
+                if (err) { request.logger.error(err); }
+                opts.org = org;
+                return reply.view('org/info', opts);
+              });
+          });
+      });
+
+    });
+
+  }
+};
+
+Org.deleteOrg = function (request, reply) {
   var loggedInUser = request.loggedInUser && request.loggedInUser.name;
 
   Org(loggedInUser)
@@ -46,5 +128,4 @@ exp.deleteOrg = function (request, reply) {
     });
 };
 
-
-module.exports = exp;
+module.exports = exports;

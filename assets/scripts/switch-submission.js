40fd0b5e4f33a0228cce08a6f95564d87adb1580
Jeffrey Lembeck <jlembeck@gmail.com>
2015-09-16 13:16:12 -0700

    Move switch click handling up to table level

--- assets/scripts/switch-submission.js
+++ assets/scripts/switch-submission.js
@@ -1,28 +1,23 @@
 var formToRequestObject = require("./form-to-request-object");
 
-var Switch = function(el) {
-  this.el = el;
-  this.$el = $(el);
-  this.form = this.$el.closest(".edit-pay-status");
-};
-
-Switch.prototype.bindEvents = function() {
-  this.$el.on("click", function(e) {
-    $.ajax(formToRequestObject(this.form))
-      .done(function(data) {
-        console.log(data);
-      })
-      .fail(function() {})
-  }.bind(this));
-};
-
 module.exports = function() {
   $(function() {
-    var checkboxes = $(".switch");
+    var eut = $(".org-edit-user-table");
 
-    $.each(checkboxes, function(idx, checkbox) {
-      var input = new Switch(checkbox);
-      input.bindEvents();
+    eut.on("click", "input[type=checkbox].switch", function(e) {
+      var checkbox = $(this);
+      var checked = checkbox.prop("checked");
+      var form = checkbox.closest("form");
+
+      $.ajax(formToRequestObject(form))
+        .done(function(data) {})
+        .fail(function() {
+          if (checked) {
+            checkbox.removeAttr("checked");
+          } else {
+            checkbox.attr("checked", "checked");
+          }
+        });
     });
   });
 

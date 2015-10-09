1397d29681836790536be9d67278fe8729579307
Aria Stewart <aredridel@dinhe.net>
2015-10-09 13:22:41 -0400

    A new approach to controlling constructor.io styles

--- assets/scripts/vendor/constructor-io.js
+++ assets/scripts/vendor/constructor-io.js
@@ -1,19 +1,25 @@
 $(function() {
 
-  var searchWidth = $('#npm-search').outerWidth() - $('#site-search-submit').outerWidth();
+  var searchWidth;
 
+  var style = document.createElement('style');
+  document.head.appendChild(style);
+
+  $(window).resize(init);
+
+  init();
   $.getScript("//cnstrc.com/js/ac.js", function() {
+
     $('#site-search').constructorAutocomplete({
       key: 'CD06z4gVeqSXRiDL2ZNK',
       directResults: true,
-      maxHeight: 400,
-      width: searchWidth
+      maxHeight: 400
     });
   });
 
-  $(window).resize(function() {
+  function init() {
     searchWidth = $('#npm-search').outerWidth() - $('#site-search-submit').outerWidth();
-    $('.autocomplete-suggestions').outerWidth(searchWidth);
-  });
+    style.innerHTML = ".autocomplete-container, .autocomplete-suggestions, #powered-by-constructor-io { width: " + searchWidth + "px !important }";
+  }
 });
 

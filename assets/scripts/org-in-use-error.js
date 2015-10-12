d3599c6dcb74eb709017a91d28fca108baf5316c
Jeffrey Lembeck <jlembeck@gmail.com>
2015-10-12 16:20:10 -0700

    Start beginning of validator plugin

--- assets/scripts/org-in-use-error.js
+++ assets/scripts/org-in-use-error.js
@@ -1,13 +1,29 @@
 module.exports = function() {
-  $(function() {
+
+  $.fn.setupValidation = function() {
+    var userName = $("[data-user-name]").data("user-name");
     var $continueBtn = $('input[value="continue"]');
 
     if ($continueBtn.data('inUseError')) {
       $continueBtn.attr('disabled', 'disabled');
-
-      $('#orgScope').focus(function() {
-        $continueBtn.removeAttr('disabled');
-      });
     }
+
+    $.each(this, function() {
+      var $this = $(this);
+
+      if ($this.data("validate") === "org-scope") {
+        $(this).on("keyup", function(e) {
+          if ($(this).val() !== userName) {
+            $continueBtn.removeAttr('disabled');
+          } else {
+            $continueBtn.attr('disabled', 'disabled');
+          }
+        });
+      }
+    });
+  };
+
+  $(function() {
+    $("[data-validate]").setupValidation();
   });
 };

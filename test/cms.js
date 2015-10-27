c8211b5825ed5249e863af09a5fe18f8a27fab99
Aria Stewart <aredridel@dinhe.net>
2015-10-27 12:41:10 -0400

    Work in a redis pool for CMS

--- test/cms.js
+++ test/cms.js
@@ -11,16 +11,31 @@ var Code = require('code'),
 
 var fixture = require('./fixtures/cms/testPage.json');
 
+var requireInject = require('require-inject');
+
 var nock = require('nock');
 
 process.env.CMS_API = 'http://cms-api/npm/v1/';
 
-var CMS = require('../lib/cms');
+var CMS = requireInject('../lib/cms', {
+  redis: require('redis-mock')
+});
 
 describe('CMS', function() {
   it('loads a page', function(done) {
     var cmsMock = nock(process.env.CMS_API).get('/pages/test-page').reply(200, fixture);
-    CMS('test-page').then(function() {
+    CMS('test-page').then(function(page) {
+      expect(page.fetchedAt).to.be.a.number();
+      expect(page.fetchedFromCacheAt).to.not.exist();
+      cmsMock.done();
+      done()
+    }, done);
+  });
+
+  it('loads a page again', function(done) {
+    CMS('test-page').then(function(page) {
+      expect(page.fetchedAt).to.be.a.number();
+      expect(page.fetchedFromCacheAt).to.be.a.number();
       done()
     }, done);
   });

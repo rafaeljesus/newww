2201e8b6591d3196dee8f014e838d51459946973
Aria Stewart <aredridel@dinhe.net>
2015-12-15 15:41:24 -0800

    Update to the new package agent API, and remove extraneous logging

--- handlers/tutorial.js
+++ handlers/tutorial.js
@@ -1,20 +1,17 @@
 var npa = require('npm-package-arg');
-var PackageModel = require("../models/package");
+var PackageAgent = require("../agents/package");
 
 module.exports = function(request, reply) {
-	console.log(request.params)
-    var name = request.packageName;
-    console.log(JSON.stringify(name));
-    console.log(name)
-    var Package = PackageModel.new(request);
-    console.log("here1");
+  var name = request.packageName;
+  var Package = new PackageAgent(request.loggedInUser);
 
-    Package.get(name)
+  Package.get(name)
     .then(function(pkg) {
-    	console.log("here2");
-        var context = { package: pkg };
-        reply.view('package/tutorial',context);
-    }).catch(function(err) {
-        console.log(err);
+      var context = {
+        package: pkg
+      };
+      reply.view('package/tutorial', context);
+    }, function(err) {
+      return reply(err);
     });
 };

5f1397ffaeaaeda47fcc53d9b005c8b6bad9ddf5
Aria Stewart <aredridel@dinhe.net>
2015-12-29 17:33:07 -0500

    Rework packages handler to be a simpler maze of logic

--- handlers/package.js
+++ handlers/package.js
@@ -21,95 +21,81 @@ exports.show = function(request, reply) {
 
   request.logger.info('get package: ' + name);
 
-  var actions = {};
-  actions.package = Package.get(name);
-  actions.dependents = Package.list({
-    dependency: name,
-    limit: 50
-  }, DEPENDENCY_TTL);
-  if (!feature('npmo')) {
-    actions.downloads = Download.getAll(name);
-  }
+  P.all([
+    Package.get(name),
+    Package.list({
+      dependency: name,
+      limit: 50
+    }, DEPENDENCY_TTL),
+    feature('npmo') ? null : Download.getAll(name),
+  ]).spread(function(pkg, dependents, downloads) {
+    pkg.dependents = dependents;
+    if (pkg.name[0] != '@') {
+      pkg.downloads = downloads;
+    }
 
-  P.props(actions)
-    .then(function(results) {
-      var pkg = results.package;
-      pkg.dependents = results.dependents;
-      if (pkg.name[0] != '@') {
-        pkg.downloads = results.downloads;
+    if (pkg && pkg.time && pkg.time.unpublished) {
+      request.logger.info('package is unpublished: ' + name);
+      return reply.view('package/unpublished', context).code(404);
+    }
+
+    if (_.get(pkg, 'dependents.results.length')) {
+      pkg.numMoreDependents = pkg.dependentCount - pkg.dependents.results.length;
+    }
+
+    pkg.isStarred = Boolean(loggedInUser)
+      && Array.isArray(pkg.stars)
+      && pkg.stars.indexOf(loggedInUser.name) > -1;
+
+    pkg.isCollaboratedOnByUser = Boolean(loggedInUser)
+      && (typeof pkg.collaborators === "object")
+      && (loggedInUser.name in pkg.collaborators);
+
+    pkg.hasStats = pkg.downloads || (pkg.bugs && pkg.bugs.url) || (pkg.pull_requests && pkg.pull_requests.url);
+
+    if (!feature('npmo')) {
+      pkg.marketing = marketingblob(request.query.marketing);
+    }
+
+    context.package = pkg;
+    return reply.view('package/show', context);
+
+  }).catch(function(err) {
+    // unpaid collaborator
+    if (err.statusCode === 402) {
+      return reply.redirect('/settings/billing?package=' + name);
+    }
+
+    if (err.statusCode === 404) {
+      var pkg = npa(name);
+      pkg.available = false;
+
+      if (!validate(name).validForNewPackages) {
+        context.package = pkg;
+        return reply.view('errors/package-not-found', context).code(400);
       }
 
-      if (pkg && pkg.time && pkg.time.unpublished) {
-        request.logger.info('package is unpublished: ' + name);
-        reply.view('package/unpublished', context).code(404);
-        return;
+      if (pkg.scope) {
+        pkg.owner = pkg.scope.slice(1);
       }
 
-      if (_.get(pkg, 'dependents.results.length')) {
-        pkg.numMoreDependents = pkg.dependentCount - pkg.dependents.results.length;
+      if (!pkg.scope || (loggedInUser && pkg.owner === loggedInUser.name)) {
+        pkg.available = true;
+      } else if (loggedInUser) {
+        pkg.unavailableToLoggedInUser = true;
+      } else {
+        pkg.unavailableToAnonymousUser = true;
       }
 
-      pkg.isStarred = Boolean(loggedInUser)
-        && Array.isArray(pkg.stars)
-        && pkg.stars.indexOf(loggedInUser.name) > -1;
-
-      pkg.isCollaboratedOnByUser = Boolean(loggedInUser)
-        && (typeof pkg.collaborators === "object")
-        && (loggedInUser.name in pkg.collaborators);
-
-      pkg.hasStats = pkg.downloads || (pkg.bugs && pkg.bugs.url) || (pkg.pull_requests && pkg.pull_requests.url);
-
-      if (!feature('npmo')) {
-        pkg.marketing = marketingblob(request.query.marketing);
-      }
       context.package = pkg;
 
-      return reply.view('package/show', context);
+      return reply.view('errors/package-not-found', context).code(404);
+    }
 
-    })
-    .catch(function(err) {
-      // unpaid collaborator
-      if (err.statusCode === 402) {
-        reply.redirect('/settings/billing?package=' + name);
-        
-        return;
-      }
-
-      if (err.statusCode === 404) {
-        var pkg = npa(name);
-        pkg.available = false;
-
-        if (!validate(name).validForNewPackages) {
-          context.package = pkg;
-          reply.view('errors/package-not-found', context).code(400);
-          return;
-        }
-
-        if (pkg.scope) {
-          pkg.owner = pkg.scope.slice(1);
-          if (loggedInUser) {
-            if (pkg.owner === loggedInUser.name) {
-              pkg.available = true;
-            } else {
-              pkg.unavailableToLoggedInUser = true;
-            }
-          } else {
-            pkg.unavailableToAnonymousUser = true;
-          }
-        } else {
-          pkg.available = true;
-        }
-
-        context.package = pkg;
-        reply.view('errors/package-not-found', context).code(404);
-        return;
-      }
-
-      request.logger.error(err);
-      err.statusCode = 500;
-      reply(err);
-      return;
-    });
+    request.logger.error(err);
+    err.statusCode = 500;
+    return reply(err);
+  });
 };
 
 exports.update = function(request, reply) {

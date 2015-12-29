dd17eba02efd368e775408b980b9fefbb48c680f
Aria Stewart <aredridel@dinhe.net>
2015-12-29 12:36:38 -0500

    Extract the json handling from the specifics of pages

--- agents/cms.js
+++ agents/cms.js
@@ -13,19 +13,23 @@ function fetchPage(slug) {
   var pageRoot = url.resolve(process.env.CMS_API, 'pages/');
   var pageUrl = url.resolve(pageRoot, slug);
   debug("Fetching %j for %j", pageUrl, slug);
-  return fetch(pageUrl).then(function(res) {
-    if (res.status >= 300) {
-      var err = new Error("Bad status: " + res.status);
-      err.statusCode = res.status;
-      throw err;
-    }
-    return res.json()
-  }).then(function(page) {
+  return fetchAndDecode(pageUrl).then(function(page) {
     debug("Got content for %j: %j", slug, page);
     if (typeof page != 'object' || !page.id || !page.html || !page.title) {
       throw new Error("Invalid page returned");
     }
     return page;
+  });
+}
+
+function fetchAndDecode(url) {
+  return fetch(url).then(function(res) {
+    if (res.status >= 300) {
+      var err = new Error("Bad status: " + res.status);
+      err.statusCode = res.status;
+      throw err;
+    }
+    return res.json();
   }).then(function addMarker(json) {
     json.fetchedAt = Date.now();
     return json;

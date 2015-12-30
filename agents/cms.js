11232cea3b12f9b672c59641852d6e263ee48e6e
Aria Stewart <aredridel@dinhe.net>
2015-12-29 19:22:34 -0500

    Use a separate cache for promos

--- agents/cms.js
+++ agents/cms.js
@@ -9,7 +9,7 @@ var Cache = require('../lib/background-refresh-cache');
 var debug = require('debuglog')('newww:cms');
 
 var pageCache = new Cache('content', fetchPage, process.env.CMS_CACHE_TIME || 30 * 60);
-var promotionCache = new Cache('content', fetchPromotion, process.env.CMS_CACHE_TIME || 30 * 60);
+var promotionCache = new Cache('promotions', fetchPromotion, process.env.CMS_CACHE_TIME || 30 * 60);
 
 function fetchPage(slug) {
   var pageRoot = url.resolve(process.env.CMS_API, 'pages/');

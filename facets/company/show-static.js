926ee131732c7cde8a88c1ea4dec2908b1d6b0ef
rockbot <raquel@rckbt.me>
2016-01-26 17:13:50 -0800

    removes unused code (static pages)
    this code has been replaced by the wordpress instance/marketing pages

--- facets/company/show-static.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(type, text) {
-  return function(request, reply) {
-    var opts = { };
-    opts.text = text;
-    request.timing.page = 'static-' + type;
-    reply.view('static', opts);
-  };
-};

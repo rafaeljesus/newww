5fcdfa0da6e51349d32f267acb28c115c691b389
rockbot <raquel@rckbt.me>
2016-02-01 15:11:37 -0800

    tidies up create-org test

--- integration/create-org.js
+++ integration/create-org.js
@@ -1,6 +1,5 @@
 var tap = require('tap');
 var urlOf = require('./lib/url');
-var pass = require('./lib/pass');
 var P = require('bluebird');
 
 require('./lib/sharedNemo').then(function(nemo) {
@@ -11,36 +10,35 @@ require('./lib/sharedNemo').then(function(nemo) {
   tap.test('create org', {
     bail: true
   }, function(t) {
-    return P.all([
-      nemo.driver.get(urlOf('/org/create')),
-      nemo.view.createOrg.h1TextEquals('Create an Organization').then(pass(t, "Got to the right page")),
-      nemo.view.createOrg.fullnameWaitVisible().then(pass(t, "Found form field")),
-      nemo.view.createOrg.fullname().sendKeys(nemo.state.desiredUsername + "-org"),
-      nemo.view.createOrg.orgScope().sendKeys(nemo.state.desiredOrgScope),
-      nemo.view.createOrg.createOrgFormSubmit().click()
-    ]).then(function() {
-      return P.all([
-        nemo.view.createOrg.h1WaitVisible().then(pass(t, "Billing org page navigated to")),
-        nemo.view.createOrg.h1TextEquals('Billing information'),
-        nemo.view.createOrg.cardNumber().sendKeys('4242 4242 4242 4242').then(pass(t, "card number entered")),
-        nemo.view.createOrg.cardCVC().sendKeys('434').then(pass(t, "cvc entered")),
-        nemo.view.createOrg.cardExpMonth().sendKeys('8').then(pass(t, "expiration month entered")),
-        nemo.view.createOrg.cardExpYear().sendKeys(new Date().getFullYear() + 3).then(pass(t, "exp year entered")),
-        nemo.view.createOrg.paymentFormSubmit().click()
-      ])
-    }).then(function() {
-      return P.all([
-        nemo.view.createOrg.h2WaitVisible().then(pass(t, "Packages header visible")),
-        nemo.view.createOrg.h2TextEquals('0 packages'),
-        nemo.view.nav.username().getText().then(textEquals(t, nemo.state.desiredUsername)).then(pass(t, "Username correct")),
-        nemo.view.createOrg.h1TextEquals("@" + nemo.state.desiredOrgScope).then(pass(t, "Org name correct")),
-      ])
-    }).then(t.ok).catch(t.error).then(t.end);
+    return nemo.driver.get(urlOf('/org/create')).then(() => P.all([
+        nemo.view.createOrg.h1TextEquals('Create an Organization').then(() => t.pass("Got to the right page")),
+        nemo.view.createOrg.fullnameWaitVisible().then(() => t.pass("Found form field")),
+        nemo.view.createOrg.fullname().sendKeys(nemo.state.desiredUsername + "-org"),
+        nemo.view.createOrg.orgScope().sendKeys(nemo.state.desiredOrgScope),
+        nemo.view.createOrg.createOrgFormSubmit().click()
+      ]).then(function() {
+        return P.all([
+          nemo.view.createOrg.h1WaitVisible().then(() => t.pass("Billing org page navigated to")),
+          nemo.view.createOrg.h1TextEquals('Billing information'),
+          nemo.view.createOrg.cardNumber().sendKeys('4242 4242 4242 4242').then(() => t.pass("card number entered")),
+          nemo.view.createOrg.cardCVC().sendKeys('434').then(() => t.pass("cvc entered")),
+          nemo.view.createOrg.cardExpMonth().sendKeys('8').then(() => t.pass("expiration month entered")),
+          nemo.view.createOrg.cardExpYear().sendKeys(new Date().getFullYear() + 3).then(() => t.pass("exp year entered")),
+          nemo.view.createOrg.paymentFormSubmit().click()
+        ]);
+      }).then(function() {
+        return P.all([
+          nemo.view.createOrg.h2WaitVisible().then(() => t.pass("Packages header visible")),
+          nemo.view.createOrg.h2TextEquals('0 packages'),
+          nemo.view.nav.username().getText().then(textEquals(t, nemo.state.desiredUsername)).then(() => t.pass("Username correct")),
+          nemo.view.createOrg.h1TextEquals("@" + nemo.state.desiredOrgScope).then(() => t.pass("Org name correct")),
+        ]);
+      }));
   });
 });
 
 function textEquals(t, expected) {
   return function(text) {
     t.equals(expected, text);
-  }
+  };
 }

8d08dc28592c3bfbef243832f947b52b573a34d4
rockbot <raquel@rckbt.me>
2016-02-02 15:27:32 -0800

    removes email service from plugins

--- adapters/plugins.js
+++ adapters/plugins.js
@@ -16,7 +16,6 @@ module.exports = [
   },
   require('../services/user'),
   require('../services/corporate'),
-  require('../services/email'),
   require('../services/npme'),
   {
     register: require('./bonbon'),
